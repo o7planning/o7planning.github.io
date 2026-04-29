b291cf66b06a133d475692e061fd612d9c8787bb
rollback <rollback@rollbacks-M.local>
2026-04-29 23:18:16 +0600

    .

---
+++ flutter-artist/firebase-messaging-sw-ok.js
@@ -0,0 +1,42 @@
+// Import scripts for Firebase Core and Messaging
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker
+// You can find these values in your firebase_options.dart or Firebase Console
+firebase.initializeApp({
+  apiKey: "AIzaSyDlwdYOb43O6NP6-vZWH-n5hPdF3L7Zf6M",
+  authDomain: "flutterartistdemo.firebaseapp.com",
+  projectId: "flutterartistdemo",
+  storageBucket: "flutterartistdemo.firebasestorage.app",
+  messagingSenderId: "1036875611970",
+  appId: "1:1036875611970:web:d5ac291448ca368b6412db"
+});
+
+
+
+const messaging = firebase.messaging();
+
+// Optional: Handle background messages
+messaging.onBackgroundMessage((payload) => {
+console.log('Received background message ', payload);
+const notificationTitle = payload.notification.title;
+const notificationOptions = {
+body: payload.notification.body,
+};
+
+self.registration.showNotification(notificationTitle, notificationOptions);
+
+// Force the service worker to take control immediately
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', () => {});
+
+});
+
