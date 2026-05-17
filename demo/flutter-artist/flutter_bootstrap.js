6e58820b23c627d5ae68a5aef36c41d0abac700c
rollback <rollback@rollbacks-M.local>
2026-05-17 19:01:43 +0600

    .

--- demo/flutter-artist/flutter_bootstrap.js
+++ demo/flutter-artist/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "523416472" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "167482341" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
